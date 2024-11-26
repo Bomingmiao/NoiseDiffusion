245e6453a02866bb11fc9b8c3da749fe2605da0c
bomingmiao <bomingmiao@gmail.com>
2024-11-26 17:38:44 +0800

    updateupdate

--- run.py
+++ run.py
@@ -1,18 +1,16 @@
-import t2v_metrics
-import torch
-import json
+import os
+import numpy as np
 from PIL import Image
+import torch
 from torchvision import transforms
 import torch.nn.functional as F
-import numpy as np
 from diffusers import AutoencoderKL, UNet2DConditionModel,DDIMScheduler,DDPMScheduler,PNDMScheduler
 from transformers import CLIPTextModel, AutoTokenizer
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import os
+import t2v_metrics
 
 
-def generate_logo(prompt,tokenizer,text_encoder,vae,unet,scheduler,latents= None,uncond_embeddings_list=None,seed=33,size=512, num_inference_steps=50,guidance_scale=7.5,start_step=0,device='cuda:0'):
+def generate_img(prompt,tokenizer,text_encoder,vae,unet,scheduler,latents= None, seed=33,size=512, num_inference_steps=50,guidance_scale=7.5,start_step=0,device='cuda:0'):
     device = torch.device(device)
     uncond_input = tokenizer(
         "",
@@ -41,9 +39,6 @@ def generate_logo(prompt,tokenizer,text_encoder,vae,unet,scheduler,latents= None
     all_latents = [latents_init]
     for i,t in tqdm(enumerate(scheduler.timesteps[start_step:]),total=len(scheduler.timesteps[start_step:]),desc="generating images with prompts"):
         latents_input = torch.cat([latents]*2)
-        #noise_pred = unet(latents_input,t,encoder_hidden_states=context)['sample']
-        if uncond_embeddings_list is not None:
-            context = torch.cat([uncond_embeddings_list[i+start_step],cond_embeddings])
         noise_pred = unet(latents_input,t,context)['sample']
         noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
         noise_pred = noise_pred_uncond + guidance_scale*(noise_pred_cond - noise_pred_uncond)
@@ -51,10 +46,10 @@ def generate_logo(prompt,tokenizer,text_encoder,vae,unet,scheduler,latents= None
         latents = scheduler.step(noise_pred,t,latents)['prev_sample']
         all_latents.append(latents.clone().detach())
     latents_last = 1/vae.config.scaling_factor *latents
-    logos = vae.decode(latents_last)['sample']
-    logos = logos * 0.5 + 0.5
-    logos = logos.clamp(0,1)
-    return logos, latents, all_latents,noise_list,uncond_embeddings
+    imgs = vae.decode(latents_last)['sample']
+    imgs = imgs * 0.5 + 0.5
+    imgs = imgs.clamp(0,1)
+    return imgs, latents, all_latents,noise_list,uncond_embeddings
 def reverse(latent, noise_list,scheduler,start_step=0):
     for i,t in tqdm(enumerate(scheduler.timesteps[start_step:]),total = len(scheduler.timesteps[start_step:]),desc="reverse noisy sample with predicted noise"):
         noise = noise_list[i]
@@ -67,7 +62,6 @@ def latent2img(vae,latent):
     img = img.clamp(0,1)
     return img
 def load_checkpoint(checkpoint_path,device):
-    # Load scheduler and models
     text_encoder = CLIPTextModel.from_pretrained(
         checkpoint_path,
         subfolder="text_encoder",
@@ -93,105 +87,56 @@ def load_checkpoint(checkpoint_path,device):
     return text_encoder, tokenizer, unet, vae, ddim_scheduler
 
 def main():
+    SD_VERSION = "CompVis/stable-diffusion-v1-4"
+    seeds = [33]
+    prompt = "a book on the sofa"
     device = 'cuda:0'
-    result_root='simple'
+    result_root='results'
+    num_candidate_noises = 50
+    num_denoising_steps = 50
+    max_epochs = 50
     to_pil = transforms.ToPILImage()
+    detail = [prompt]
     vqa_model = t2v_metrics.VQAScore(model='clip-flant5-xxl',device=device)
-    #vqa_model = t2v_metrics.VQAScore(model='llava-v1.5-13b')
-    #vqa_model = t2v_metrics.VQAScore(model='instructblip-flant5-xxl')
     vqa_model.model.model.requires_grad_(False)
-    clip_model = t2v_metrics.CLIPScore(model='laion2b_s32b_b82k:ViT-L-14',device=device)
-    clip_model.model.model.requires_grad_(False)
-    version_list = {"v-1-4":"CompVis/stable-diffusion-v1-4",
-                       "v-1-5":"stable-diffusion-1-5",
-                       "v-2-0":"stable-diffusion-2",
-                       "v-2-1":"stable-diffusion-2-1"}
-    versions = ["v-1-4"]
-    #seeds = [33, 5599, 9423 , 7409, 4416, 2359, 1142, 5266, 5696, 2754, 4161]
-    seeds  = [22,15,9951,31,9989]
-    for version in versions:
-        checkpoint_path = version_list[version]
-        text_encoder, tokenizer, unet, vae, ddim_scheduler = load_checkpoint(checkpoint_path,device)
-        #with open('new_prompts.json', 'r') as f:
-        #    prompts = json.load(f)
-        #prompts = [dict['prompt']for dict in prompts]
+    text_encoder, tokenizer, unet, vae, ddim_scheduler = load_checkpoint(SD_VERSION,device)
 
-        with open('a.e_prompts.json', 'r') as f:
-            dicts = json.load(f)
-        for cls in ['animals', 'animals_objects', 'objects']:
-            prompts = dicts[cls]
-            for dict in prompts:
-                prompt = dict['prompt']
-                if prompt in ["a bear and a rabbit",
-                                "a lion and a monkey"
-                                "a purple crown and a blue bench"]:
-                    detail = [prompt]
-                    for seed in seeds:
-                        dir = f"{result_root}/{cls}/{prompt.replace(' ', '_')}/{version}/{seed}/sd"
-                        os.makedirs(dir, exist_ok=True)
-                        logo, latents, all_latents,noise_list,_ = generate_logo(prompt,tokenizer,text_encoder,vae,unet,ddim_scheduler,latents = None,size=512,seed=seed,num_inference_steps=50,guidance_scale=7.5,device=device)
-                        target = to_pil(logo.squeeze(0))
-                        target.save(f'{dir}/0.png')
-                        latent_init = all_latents[0].detach()
-                        latent_init.requires_grad = True
-                        latent = reverse(latent_init, noise_list, ddim_scheduler)
-                        logo = latent2img(vae, latent)
-                        vqa_score = vqa_model(logo, detail)
-                        with torch.no_grad():
-                            clip_score = clip_model(logo, detail)
-                        max_vqa_score = vqa_score.item()
-                        max_clip_score = clip_score.item()
-                        vqa_scores = [vqa_score.item()]
-                        clip_scores = [clip_score.item()]
-                        max_vqa_scores = [max_vqa_score]
-                        max_clip_scores = [max_clip_score]
-                        for i in range(50):
-                            vqa_score.backward()
-                            step_lr = 1 - vqa_score.item()**(1/2)
-
-
-                            grad = latent_init.grad
-                            noise_pool = torch.randn(50, *grad.shape).to(grad.device)
-                            sims = []
-                            for noise in noise_pool:
-                                vec = ((1 - step_lr) ** 0.5 - 1) * latent_init.detach() + step_lr ** 0.5 * noise
-                                sim = (vec  * grad ).sum() / vec.norm(2)**2
-                                sims.append(sim.item())
-                            noise = noise_pool[np.argmax(sims)]
-
-
-                            latent_tmp = latent_init.detach()
-                            latent_tmp =  (1-step_lr)**0.5 * latent_tmp + step_lr**0.5 * noise
-
-
-                            latent_init = latent_tmp.detach()
-                            _, _, _,noise_list,_ = generate_logo(prompt,tokenizer,text_encoder,vae,unet,ddim_scheduler,latents = latent_init,size=512,seed=seed,num_inference_steps=50,guidance_scale=7.5,device=device)
-                            latent_init.requires_grad = True
-
-                            latent = reverse(latent_init, noise_list, ddim_scheduler)
-                            logo = latent2img(vae, latent)
-                            vqa_score = vqa_model(logo, detail)
-                            with torch.no_grad():
-                                clip_score = clip_model(logo, detail)
-                            image = to_pil(logo.squeeze(0))
-                            if vqa_score > max_vqa_score:
-                                target = image
-                                max_vqa_score = vqa_score.item()
-                            if clip_score > max_clip_score:
-                                max_clip_score = clip_score.item()
-                            vqa_scores.append(vqa_score.item())
-                            max_vqa_scores.append(max_vqa_score)
-                            clip_scores.append(clip_score.item())
-                            max_clip_scores.append(max_clip_score)
-                            image.save(f'{dir}/{i+1}.png')
-                        target.save(f'{dir}/target.png')
-                        np.save(f'{dir}/vqa_scores.npy', np.array(vqa_scores))
-                        np.save(f'{dir}/max_vqa_scores.npy', np.array(max_vqa_scores))
-                        np.save(f'{dir}/clip_scores.npy', np.array(clip_scores))
-                        np.save(f'{dir}/max_clip_scores.npy', np.array(max_clip_scores))
-                        del clip_score
-                        del vqa_score
-
-
+    for seed in seeds:
+        dir = f"{result_root}/{prompt.replace(' ', '_')}/{seed}"
+        os.makedirs(dir, exist_ok=True)
+        img, latents, all_latents,noise_list,_ = generate_img(prompt,tokenizer,text_encoder,vae,unet,ddim_scheduler,latents = None,size=512,seed=seed,num_inference_steps=num_denoising_steps,guidance_scale=7.5,device=device)
+        target = to_pil(img.squeeze(0))
+        target.save(f'{dir}/0.png')
+        latent_init = all_latents[0].detach()
+        latent_init.requires_grad = True
+        latent = reverse(latent_init, noise_list, ddim_scheduler)
+        img = latent2img(vae, latent)
+        vqa_score = vqa_model(img, detail)
+        max_vqa_score = vqa_score.item()
+        for i in range(max_epochs):
+            vqa_score.backward()
+            step_lr = 1 - vqa_score.item()**(1/2)
+            grad = latent_init.grad
+            noise_pool = torch.randn(num_candidate_noises, *grad.shape).to(grad.device)
+            sims = []
+            for noise in noise_pool:
+                vec = ((1 - step_lr) ** 0.5 - 1) * latent_init.detach() + step_lr ** 0.5 * noise
+                sim = (vec  * grad ).sum() / vec.norm(2)**2
+                sims.append(sim.item())
+            noise = noise_pool[np.argmax(sims)]
+            latent_tmp =  (1-step_lr)**0.5 * latent_init + step_lr**0.5 * noise
+            latent_init = latent_tmp.detach()
+            _, _, _,noise_list,_ = generate_img(prompt,tokenizer,text_encoder,vae,unet,ddim_scheduler,latents = latent_init,size=512,seed=seed,num_inference_steps=num_denoising_steps,guidance_scale=7.5,device=device)
+            latent_init.requires_grad = True
+            latent = reverse(latent_init, noise_list, ddim_scheduler)
+            img = latent2img(vae, latent)
+            vqa_score = vqa_model(img, detail)
+            image = to_pil(img.squeeze(0))
+            image.save(f'{dir}/{i+1}.png')
+            if vqa_score > max_vqa_score:
+                target = image
+                max_vqa_score = vqa_score.item()
+        del vqa_score
+        target.save(f'{dir}/target.png')
 if __name__=='__main__':
     main()
